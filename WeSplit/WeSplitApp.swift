0dc6a32f4de223e19a8d588f3cb4be9689f0d73b
SantiGO055 <santigonzalez05@gmail.com>
2023-11-29 22:00:53 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Santiago Gonzalez on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
