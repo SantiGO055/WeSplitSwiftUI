a108b7cbb9d24c837df82e25fabf394e934ff612
SantiGO055 <santigonzalez05@gmail.com>
2023-12-10 15:05:50 -0300

    .

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,32 +13,8 @@ struct ContentView: View {
     
     // So, when you see a dollar sign before a property name, remember that it creates a two-way binding: the value of the property is read, but also written
     
-    @State private var tapCount = 0 //Apple recomienda ponerla privada
-    @State private var name = ""
-    let students = ["Harry","Hermione","Ron"]
-    @State private var selectedStudent = "Harry"
     var body: some View {
-        
-        NavigationStack {
-                Form {
-                    Section {
-                        TextField("Enter your name here", text: $name) // the $name is for two ways binding, show it and write variable
-                        
-                        Text("Your name is \(name)")
-                        
-                    }
-                    Button("Tap Count: \(tapCount)") {
-                           tapCount += 1
-                       }
-                    Picker("Select your student",selection: $selectedStudent){
-                        ForEach(students, id: \.self){ // Significa el id es el mismo string. Este id es para identificar en la lista un unico valor por si llega a cambiar
-                            Text($0)
-                        }
-                    }
-                }.navigationTitle("SwiftUI")
-                .navigationBarTitleDisplayMode(.inline)
-            
-        }
+        Text("Hello, world!")
     }
 }
 
