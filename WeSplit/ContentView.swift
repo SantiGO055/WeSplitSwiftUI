69bbed14de61d534770842760899513577860313
SantiGO055 <santigonzalez05@gmail.com>
2023-12-10 14:51:14 -0300

    Adding picker and array with foreach

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,20 +10,34 @@ import SwiftUI
 struct ContentView: View {
     // var tapCount = 0 si hago esto no puedo por que el struct es inmutable
     // tengo que declararlo como mutable pero no puedo con la vista. Se usa esto:
-    @State private var tapCount = 0 //Apple recomienda ponerla privada
     
+    // So, when you see a dollar sign before a property name, remember that it creates a two-way binding: the value of the property is read, but also written
+    
+    @State private var tapCount = 0 //Apple recomienda ponerla privada
+    @State private var name = ""
+    let students = ["Harry","Hermione","Ron"]
+    @State private var selectedStudent = "Harry"
     var body: some View {
+        
         NavigationStack {
                 Form {
                     Section {
-                        Text("Hello, world!")
+                        TextField("Enter your name here", text: $name) // the $name is for two ways binding, show it and write variable
+                        
+                        Text("Your name is \(name)")
                         
                     }
                     Button("Tap Count: \(tapCount)") {
                            tapCount += 1
                        }
+                    Picker("Select your student",selection: $selectedStudent){
+                        ForEach(students, id: \.self){ // Significa el id es el mismo string. Este id es para identificar en la lista un unico valor por si llega a cambiar
+                            Text($0)
+                        }
+                    }
                 }.navigationTitle("SwiftUI")
                 .navigationBarTitleDisplayMode(.inline)
+            
         }
     }
 }
