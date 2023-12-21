0b102179afeb08f16cf150efe567132e3884a245
SantiGO055 <santigonzalez05@gmail.com>
2023-12-20 23:52:27 -0300

    adding red foreground to total amount if user select 0% of tip

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -77,6 +77,7 @@ struct ContentView: View {
                 }
                 Section("Total amount") {
                     Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .black)
                 }
                 
             }
